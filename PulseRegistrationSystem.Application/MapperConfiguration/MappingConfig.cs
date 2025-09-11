f2f6fa4674d3aaf98a39183b3762fa2c6c02f4e3
Gabriela-Reiss <gabrielaasreiss@gmail.com>
2025-09-11 20:27:42 -0300

    feat: desenvolve tratativas de exceções

--- PulseRegistrationSystem.Application/MapperConfiguration/MappingConfig.cs
+++ PulseRegistrationSystem.Application/MapperConfiguration/MappingConfig.cs
@@ -9,7 +9,35 @@ public class MappingConfig : Profile
 {
     public MappingConfig()
     {
-        CreateMap<UsuarioRequestDto, Usuario>();
+        CreateMap<EnderecoRequestDto, Endereco>();
+
+        CreateMap<UsuarioRequestDto, Usuario>()
+            .ConstructUsing((dto, context) => new Usuario(
+                dto.Nome,
+                dto.Cpf,
+                dto.DataNascimento,
+                context.Mapper.Map<Endereco>(dto.FilialMottu), // aqui usa o contexto do AutoMapper
+                dto.Email,
+                dto.Funcao,
+                null
+            ));
+
+
+        CreateMap<EnderecoRequestDto, Endereco>();
+
+
+        
+        CreateMap<EnderecoRequestDto, Endereco>()
+            .ConstructUsing(dto => new Endereco(
+                dto.Rua, 
+                dto.Complemento, 
+                dto.Bairro, 
+                dto.Cep, 
+                dto.Cidade, 
+                dto.Estado
+            ));
+
+
         CreateMap<Usuario, UsuarioResponseDto>();
         CreateMap<Login, LoginResponseDto>()
             .ForMember(dest => dest.NomeUsuario, opt => opt.MapFrom(src => src.Usuario.Nome))
